029498fcae886098dc132581fc29508ab622c05a
Raghav Joshi <invoice.developer.02@gmail.com>
2023-10-11 16:10:45 +0530

    pass gen

---
+++ password_05/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useCallback, useEffect ,useRef } from "react"
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numerallow, setnumallow] = useState(false);
+  const [chrallow, setcharallow] = useState(false);
+  const [pass, setpass] = useState("")
+//ref hook
+const passRef = useRef(null)
+
+  const passgen = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHabcdefgh"
+    if (numerallow) str += "0123456789"
+    if (chrallow) str += "!@#$%^&*"
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+    setpass(pass)
+  },
+    [length, numerallow, chrallow, setpass])
+const copypass = useCallback(() =>{
+  passRef.current?.select();
+  // passRef.current?.setSelectionRange(0,3);
+  window.navigator.clipboard.writeText(pass)
+},
+[pass])
+  useEffect(() => { passgen() }, [length, numerallow, chrallow, passgen])
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-4 text-orange-500 bg-gray-700">
+        <h1 className="text-4xl text-center">password generater</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 py-4">
+          <input type="text" value={pass} className="outline-none w-full py-3 px-3" placeholder="password" readOnly ref={passRef} />
+          <button className="outline-none bg-blue-700 text-white " onClick={copypass}>copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex item-center gap-x-1">
+            <input type="range" min={6} max={100} value={length} className="cursor-pointer" onChange={(e) => { setLength(e.target.value) }} />
+            <label htmlFor="length">length{length}</label>
+          </div>
+          <input type="checkbox" defaultChecked={numerallow} id="numinput" onChange={(e) => { setnumallow((prev) => !prev) }} />
+          <label >number</label>
+          <input type="checkbox" defaultChecked={numerallow} id="numinput" onChange={(e) => { setcharallow((prev) => !prev) }} />
+          <label >char</label>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
